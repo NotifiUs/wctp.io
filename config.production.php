af373e17ddae8e3b940f75da19210fdac7a7b134
Patrick Labbett <patrick@labbett.net>
2019-11-29 21:44:07 -0500

    updated social icon for twitter cards/fb graph

--- config.production.php
+++ config.production.php
@@ -9,7 +9,7 @@ return [
     'og_site_name' => 'wctp.io',
     'og_title' => 'WCTP Gateway',
     'og_description' => 'The modern WCTP engine for call centers.',
-    'og_image' => 'https://wctp.io/assets/image/phones.svg',
+    'og_image' => 'https://wctp.io/assets/images/wctp_flow.png',
     'og_url' => 'https://wctp.io',
     'og_image_alt' => 'WCTP.io',
     'favicon' => '/assets/images/gateway.svg',
